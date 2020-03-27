679570f5c61a6e3161f7f8bd4150e26d2d4f3124
ubanitc <57474291+ubanitc@users.noreply.github.com>
2020-03-27 03:26:48 -0700

    Add files via upload

--- dbconnect.php
+++ dbconnect.php
@@ -3,7 +3,7 @@
 $server = "us-cdbr-iron-east-01.cleardb.net";
 $username = "b31804a121552f";
 $password = "a7469f36";
-$dbname = "missfinesse";
+$dbname = "heroku_10bd02a6460258e";
 
 $connect = mysqli_connect($server,$username,$password,$dbname);
 
