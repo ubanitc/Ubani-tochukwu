290e3c1fbda244ada88483fce1f2e24c2d463c9d
ubanitc <57474291+ubanitc@users.noreply.github.com>
2020-04-15 09:42:45 -0700

    Update mail1.php

--- mail1.php
+++ mail1.php
@@ -10,7 +10,7 @@ $txt = "Your Verification Code is 83935";
 $headers = "From: https://app.multitexter.com/v2/app/sendsms". "\r\n";
 
 // More headers
-$headers .= 'From: Authorization : Bearer {4vQMOzmFqiKGa7NFbqNRlzuLJ0PAYgYqQ0eCiUq4Uz9ZAtcSdW1xX65YiQBO}' . "\r\n";
+$headers .= 'From: Authorization : Bearer 4vQMOzmFqiKGa7NFbqNRlzuLJ0PAYgYqQ0eCiUq4Uz9ZAtcSdW1xX65YiQBO' . "\r\n";
 $headers .= 'Accept :application/json' . "\r\n";
 
 
